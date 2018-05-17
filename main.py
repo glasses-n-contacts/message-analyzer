e812644e18869575f9a62ecec612725c5ec878eb
Lucy Zhang <spothorse9.lucy@gmail.com>
2018-05-17 13:21:31 -0400

    Well performance went up to 66% ish, still not great

--- main.py
+++ main.py
@@ -7,15 +7,9 @@ from variables import *
 if __name__ == '__main__':
     scraper = MessageScraper(ABSOLUTE_PATH, CONTACT_INFO, NAME)
     my_texts, other_texts = scraper.all_messages()
-    print('my_texts:')
-    print(my_texts)
 
-    print('other_texts:')
-    print(other_texts)
-    print('----')
-
-    slice_my_index = int(len(my_texts) * 0.8)
-    slice_other_index = int(len(other_texts) * 0.8)
+    slice_my_index = int(len(my_texts) * 0.9)
+    slice_other_index = int(len(other_texts) * 0.9)
 
     train_my_texts = my_texts[:slice_my_index]
     train_other_texts = other_texts[:slice_other_index]
@@ -23,26 +17,12 @@ if __name__ == '__main__':
     test_my_texts = my_texts[slice_my_index:]
     test_other_texts = other_texts[slice_other_index:]
 
-    print('Test my texts:')
-    print(test_my_texts)
-
-    print('Test other texts:')
-    print(test_other_texts)
-    print('----')
-
     target_indices = ([0] * len(train_my_texts)) + ([1] * len(train_other_texts))
     test_target_indices = ([0] * len(test_my_texts)) + ([1] * len(test_other_texts))
 
     training_data = train_my_texts + train_other_texts
     test_data = test_my_texts + test_other_texts
 
-    print('Train my texts:')
-    print(train_my_texts)
-
-    print('Train other texts:')
-    print(train_other_texts)
-
-    print('----')
     targets = ['Lucy', 'Bill']
     classifier = TextClassifier(training_data, targets, target_indices)
     classifier.train('svm')
